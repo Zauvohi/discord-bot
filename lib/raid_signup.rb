bf4d18cc409db295393a461b2cdad19ed920ab15
Zauvohi <zauvohi@gmail.com>
2016-09-06 21:56:40 -0500

    Revamped RaidSignup to use arrays instead of hashes to properly store the roles (since they aren't unique). Replaced file in stick command for linl.

--- lib/raid_signup.rb
+++ lib/raid_signup.rb
@@ -2,17 +2,17 @@ class RaidSignup
 
   def new(raid_name)
     @raid_name = raid_name
-    @roles = {}
+    @roles = []
+    @users = []
   end
 
   def load_roles(roles)
-    roles.each do |role|
-      @roles[role] = 'Unassigned'
-    end
+    @roles = roles.split
+    @users = Array.new(@roles.size, 'Unassigned')
   end
 
   def already_assigned(user)
-    @roles.each do |key, value|
+    @users.each do |value|
       return true if (value === user)
     end
     return false
@@ -20,23 +20,24 @@ class RaidSignup
 
   def assign(user, role)
     if (self.already_assigned(user) === false)
-      @roles[role] = user
-    end
-  end
-
-  def unassign(user)
-    @roles.each do |key, value|
-      if (value === user)
-        @roles[key] = 'Unassigned'
+      @roles.each_with_index do |role, index|
+        if (@roles[index] === role && @users[index] === 'Unassigned')
+          @users[index] = user
+        end
       end
     end
   end
 
+  def unassign(user)
+    @users.delete(user)
+  end
+
   def roles_missing
     missing_roles = []
-    @roles.each do |key, value|
-      if (value === 'Unassigned')
-        missing_roles.push(key)
+    @roles.each_with_index do |role, index|
+      if (@users[index] === 'Unassigned')
+        missing_roles.push(role)
+      end
     end
     missing_roles.join(' ')
   end
--- lib/triadprimus_bot.rb
+++ lib/triadprimus_bot.rb
@@ -4,10 +4,10 @@ require 'yaml'
 
 bot = Discordrb::Commands::CommandBot.new token: configatron.token, application_id: configatron.appid, prefix: '!'
 @raid_details = YAML::load_file('raid_list.yaml')
-@raid_list = []
+@raid_list = {}
 
 bot.command(:stick, chain_usable: false) do |event|
-  event.channel.send_file File.new('../pics/afuckingsticklamo.png')
+  event.response "https://cdn.discordapp.com/attachments/190607069093691393/222907225809747968/a_fucking_stick_lamo.png"
 end
 
 bot.command(:make_raid, chain_usable: false) do |event, raid_name|
@@ -15,16 +15,21 @@ bot.command(:make_raid, chain_usable: false) do |event, raid_name|
     raid_signup = RaidSignup.new(raid_name)
 
     if (@raid_list.size === 15)
-      @raid_list.pop
+      @raid_list.shift
     end
 
-    raid_signup.load_roles(@raid_list[raid_name]['roles'])
+    raid_signup.load_roles(@raid_details[raid_name]['roles'])
     raid_id = Utilities.generate_id(raid_name)
-    raid = { raid_id: raid_id, raid: raid_signup }
-    @raid_list.push(raid)
+    @raid_list[raid_id] = raid_signup
     event.response "Raid id: #{raid_id}, roles missing: #{raid_signup.roles_missing}"
   else
     event.response "This raid doesn't exist, bwaka."
+  end
+end
+
+bot.command(:join_raid, chain_usable: false) do |event, raid_id, role|
+  role = role.split.join
+  @raid_list.each do |key, value|
   end
 end
 
