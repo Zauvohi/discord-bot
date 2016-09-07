e28e49d1815421cbfe54bfba3d553095932ffe78
Zauvohi <zauvohi@gmail.com>
2016-09-07 11:57:46 -0500

    Added snek, windmeme, sen and police commands. For some reason added filter to respond with a clarise picture whenever someone has 'dumb clarise' in their message.

--- lib/picture_commands.rb
+++ lib/picture_commands.rb
@@ -1,5 +1,6 @@
 module PictureCommands
   extend Discordrb::Commands::CommandContainer
+  require_relative 'utilities'
 
   command(:stick, chain_usable: false) do |event|
     event.respond "https://cdn.discordapp.com/attachments/190607069093691393/222907225809747968/a_fucking_stick_lamo.png"
@@ -7,5 +8,38 @@ module PictureCommands
 
   command(:otsu, chain_usable: false) do |event|
     event.respond "https://cdn.discordapp.com/attachments/222920939598381060/223097379279208449/8KzSio.png"
+  end
+
+  command(:snek, chain_usable: false) do |event|
+    snek_pics = [
+      "https://cdn.discordapp.com/attachments/222920939598381060/223110760073527307/1472435078016.jpg",
+      "https://cdn.discordapp.com/attachments/222920939598381060/223113005326532608/1471425508841.png",
+      "https://cdn.discordapp.com/attachments/222920939598381060/223113038935490560/1472958217093.png"
+    ]
+    event.respond Utilities.random_pic(snek_pics)
+  end
+
+  command(:windmeme, chain_usable: false) do |event|
+    wind_pics = [
+      "https://cdn.discordapp.com/attachments/222920939598381060/223112000249528321/windmeme2.jpg",
+      "https://cdn.discordapp.com/attachments/222920939598381060/223112027575549953/windmeme3.png",
+      "https://cdn.discordapp.com/attachments/222920939598381060/223111938463367168/1465766595855.jpg",
+      "https://cdn.discordapp.com/attachments/222920939598381060/223112048224108545/wind_memers.jpg",
+      "https://cdn.discordapp.com/attachments/222920939598381060/223116810612899840/1466962326898.jpg"
+    ]
+    event.respond Utilities.random_pic(wind_pics)
+  end
+
+  command(:sen, chain_usablem: false) do |event|
+    sen_pics = [
+      "https://cdn.discordapp.com/attachments/222920939598381060/223117869427195904/1470513276648.jpg",
+      "https://cdn.discordapp.com/attachments/222920939598381060/223115601932058624/SEN.jpg",
+      "https://cdn.discordapp.com/attachments/222920939598381060/223117884866560000/1470513527971.jpg"
+    ]
+    event.respond Utilities.random_pic(sen_pics)
+  end
+
+  command(:police, chain_usable: false) do |event|
+    event.respond "https://cdn.discordapp.com/attachments/222920939598381060/223115777367212037/police.jpg"
   end
 end
--- triadprimus_bot.rb
+++ triadprimus_bot.rb
@@ -3,10 +3,12 @@ require 'configatron'
 require_relative 'config'
 require_relative 'lib/picture_commands'
 require_relative 'lib/raid_commands'
+require_relative 'lib/messages'
 
 bot = Discordrb::Commands::CommandBot.new token: configatron.token, application_id: configatron.appid, prefix: '!'
 bot.include! PictureCommands
 bot.include! RaidCommands
+bot.include! Messages
 
 bot.command(:idolsheet, chain_usable: false) do |event|
   event.respond configatron.links.spreadsheet
