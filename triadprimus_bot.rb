9014b03e5d3a42be43620bf476e319d7b698a616
Zauvohi <zauvohi@gmail.com>
2016-09-07 14:36:30 -0500

    Replaced configatron with Dotenv.

--- triadprimus_bot.rb
+++ triadprimus_bot.rb
@@ -1,17 +1,18 @@
 require 'discordrb'
-require 'configatron'
+require 'dotenv'
 require_relative './lib/picture_commands'
 require_relative './config/config'
 require_relative './lib/raid_commands'
 require_relative './lib/messages'
+Dotenv.load
 
-bot = Discordrb::Commands::CommandBot.new token: configatron.token, application_id: configatron.appid, prefix: '!'
+bot = Discordrb::Commands::CommandBot.new token: ENV['TOKEN'], application_id: ENV['APPID'], prefix: '!'
 bot.include! PictureCommands
 bot.include! RaidCommands
 bot.include! Messages
 
 bot.command(:idolsheet, chain_usable: false) do |event|
-  event.respond configatron.links.spreadsheet
+  event.respond "https://docs.google.com/spreadsheets/d/13xphGFupxzXDW0Y4L0-4Da60buzetWfW_v5lonwZWG0/edit#gid=0"
 end
 
 bot.run
