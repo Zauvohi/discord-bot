33526df2c2c99d45087388fc04be40afcfe3cb10
Zauvohi <zauvohi@gmail.com>
2016-09-07 19:04:55 -0500

    Reworked how the raid signup works, to make it more fexible. Fixed issue with random_pic some times giving a nonexistant index. Added limit to pic requets. Added error handling for raid commands.

--- lib/raid_commands.rb
+++ lib/raid_commands.rb
@@ -7,49 +7,99 @@ module RaidCommands
   LIST_MAX_LENGHT = 2
   @raid_details = YAML::load_file(File.join(__dir__, 'raid_list.yml'))
   @raid_list = {}
+  @error_message = "My tummy hurts ;_;"
 
-  command(:make_raid, chain_usable: false) do |event, raid_name|
-    if (@raid_details.key?(raid_name))
-      raid_signup = RaidSignup.new(raid_name)
+  command(:create, chain_usable: false) do |event, *args|
+    begin
+      raid_name = Utilities.underscore(args)
+      if (@raid_details.key?(raid_name))
+        raid_signup = RaidSignup.new(raid_name, event.user.name)
 
-      if (@raid_list.size === LIST_MAX_LENGHT)
-        @raid_list.shift
+        if (@raid_list.size === LIST_MAX_LENGHT)
+          @raid_list.shift
+        end
+
+        raid_signup.load_roles(@raid_details[raid_name]['roles'])
+        @raid_list[raid_name] = raid_signup
+        event.respond "Raid: #{raid_signup.name}, roles suggested: #{raid_signup.suggested_roles}"
+      else
+        event.respond "This raid doesn't exist, bwaka."
       end
-
-      raid_signup.load_roles(@raid_details[raid_name]['roles'])
-      raid_id = Utilities.generate_id(raid_name)
-      @raid_list[raid_id] = raid_signup
-      event.respond "Raid id: #{raid_id}, roles missing: #{raid_signup.roles_missing}"
-    else
-      event.respond "This raid doesn't exist, bwaka."
+    rescue
+      if (args === [])
+        "Missing arugments. Usage !create [raid_name]"
+      else
+        @error_message
+      end
     end
   end
 
-  command(:join_raid, chain_usable: false) do |event, raid_id, role|
-    role = role.downcase.split.join
-    raid_id = raid_id.upcase
-    user = event.user.name
-    raid = @raid_list[raid_id]
-    if (raid.assign(user, role))
-      event.respond "User #{user} joined #{raid.name} as #{role}"
-    else
-      event.respond "#{user} you're already in this raid."
+  command(:join, chain_usable: false) do |event, *args|
+    begin
+      role = args.pop.upcase
+      raid_name = Utilities.underscore(args)
+      user = event.user.name
+      raid = @raid_list[raid_name]
+      if (raid.is_full?)
+        event.respond "Raid is full."
+      elsif (raid.add(user, role))
+        event.respond "User #{user} joined #{raid.name} as #{role}"
+      else
+        event.respond "#{user} you're already in this raid."
+      end
+    rescue
+      if (args.size < 2)
+        "Missing arguments. Usage: !join [raid_name] role(ex_skill) (eg. !join rose queen df(ar3))"
+      else
+        @error_message
+      end
     end
 
   end
-  command(:leave_raid, chain_usable: false) do |event, raid_id|
-    raid_id = raid_id.upcase
-    raid = @raid_list[raid_id]
-    raid.unassign(event.user.name)
-    event.respond "#{event.user.name} left #{raid.name}."
+  command(:leave, chain_usable: false) do |event, *args|
+    begin
+      raid_name = Utilities.underscore(args)
+      raid = @raid_list[raid_name]
+      if (raid.unassign(event.user.name))
+        event.respond "#{event.user.name} left #{raid.name}."
+      else
+        event.respond "? You aren't even in that raid, bwaka!"
+      end
+    rescue
+      if (args === [])
+        "Missing arguments. Usage !leave [raid_name]. Example: !leave rose queen"
+      else
+        @error_message
+      end
+    end
   end
 
-  command(:checkraid, chain_usable: false) do |event, raid_id|
-    raid_id = raid_id.upcase
-    raid = @raid_list[raid_id]
-    event << "Raid: #{raid.name}"
-    event << "Members joined: #{raid.users_signed}"
-    event << "Roles missing: #{raid.roles_missing}"
+  command(:check, chain_usable: false) do |event, *args|
+    begin
+      if (args === [])
+        @raid_list.each do |name, raid|
+          event << "Raid: #{raid.name} - Joined: #{raid.users_joined}/#{raid.raid_size}"
+        end
+        nil
+      else
+        raid_name = Utilities.underscore(args)
+        raid = @raid_list[raid_name]
+        event << "Raid: #{raid.name} (#{raid.users_joined}/#{raid.raid_size})"
+        event << "Members joined: #{raid.users_signed}"
+        event << "Roles suggested: #{raid.suggested_roles}"
+      end
+    rescue
+      @error_message
+    end
   end
 
+  command(:finish, chain_usable: false) do |event|
+    @raid_list.each do |name, raid|
+      if (raid.creator === event.user.name)
+        @raid_list.delete(name)
+        event.respond "https://cdn.discordapp.com/attachments/222920939598381060/223097379279208449/8KzSio.png"
+      end
+    end
+    nil
+  end
 end
--- lib/utilities.rb
+++ lib/utilities.rb
@@ -1,11 +1,15 @@
 class Utilities
-  def self.generate_id(raid_name)
-    number = rand(0 .. 999)
-    raid_name[0 .. 1].upcase + number.to_s.rjust(3, '0')
+
+  def self.underscore(string)
+    if (string.class === Array.new)
+      string.join("_").downcase
+    else
+      string.split.join("_").downcase
+    end
   end
 
   def self.random_pic(pic_array)
     pics_lenght = pic_array.size
-    return pic_array[rand(0 .. pics_lenght)]
+    return pic_array[rand(0 .. pics_lenght - 1)]
   end
 end
