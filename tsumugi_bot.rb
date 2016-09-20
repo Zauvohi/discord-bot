2ff59f1528e12b0a0ded31dfc672bccfc5a1165b
Zian <zauvohi@gmail>
2016-09-19 21:04:47 -0500

    Added superspecial command.

--- tsumugi_bot.rb
+++ tsumugi_bot.rb
@@ -14,4 +14,8 @@ bot.command(:idolsheet, chain_usable: false) do |event|
   event.respond "https://docs.google.com/spreadsheets/d/13xphGFupxzXDW0Y4L0-4Da60buzetWfW_v5lonwZWG0/edit#gid=0"
 end
 
+bot.command(:superspecial, chain_usable: false) do |event|
+  event.respond "https://www.youtube.com/watch?v=TUmm1QjLC5U"
+end
+
 bot.run
