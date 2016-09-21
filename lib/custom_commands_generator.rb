2eef526097cce78266bd78568f3385cd79da41a4
Zian <zauvohi@gmail>
2016-09-20 21:37:44 -0500

    Added custom commands. These commands are added through a command called add_command and will take 3 params; the trigger, the type (img, link, etc) and an URL. These commands will be saved in a JSON file. Removed PictureCommands and some commands in tsumugi_bot since they are not needed anymore.

---
+++ lib/custom_commands_generator.rb
@@ -0,0 +1,78 @@
+require 'json'
+require_relative 'utilities'
+
+class CustomCommandGenerator
+  DIR_LOCATION = "#{__dir__}/commands/"
+
+  def initialize(trigger, type, url, user)
+    @trigger = trigger
+    @type = type
+    @url = url
+    @commands_list = CustomCommandGenerator.load_json
+    @user = user
+  end
+
+  def log_command
+    log_path = DIR_LOCATION + 'log.txt'
+
+    unless File.exists?(log_path)
+      File.new(log_path, 'w') { |f| f << "logger:" }
+    end
+
+    File.open(log_path, 'w') do |f|
+      f << "User: #{@user} added #{@trigger} (type: #{@type}, url: #{@url})"
+    end
+  end
+
+  def self.load_json
+    file = File.read(File.join(DIR_LOCATION, 'commands.json'))
+    JSON.parse(file)
+  end
+
+  def self.load_commands(container)
+    commands = CustomCommandGenerator.load_json
+    commands.each do |command, params|
+      container.command(command.to_sym, chain_usable: false) do |event|
+        event.respond Utilities.random_element(params["urls"])
+      end
+    end
+    container
+  end
+
+  def new_command?(trigger)
+    !@commands_list.has_key?(trigger)
+  end
+
+  def new_url?(command, url)
+    !@commands_list[command]["urls"].include?(url)
+  end
+
+  def save_command(command)
+    @commands_list[@trigger] = command
+    File.open(File.join(__dir__, '/commands/commands.json'), 'w') do |f|
+      f.write(JSON.pretty_generate(@commands_list))
+    end
+    log_command
+  end
+
+  def add
+    msg = "command #{@trigger} was "
+
+    if new_command?(@trigger)
+      command = {
+        "type": @type,
+        "urls": [@url]
+      }
+      save_command(command)
+      msg << "added"
+    elsif new_url?(@trigger, @url)
+      command = @commands_list[@trigger]
+      command["urls"].push(@url)
+      save_command(command)
+      msg << "updated"
+    else
+      msg = "This url was already added."
+    end
+    msg
+  end
+end
--- tsumugi_bot.rb
+++ tsumugi_bot.rb
@@ -1,21 +1,36 @@
 require 'discordrb'
 require 'dotenv'
-require_relative './lib/picture_commands'
 require_relative './lib/raid_commands'
 require_relative './lib/messages'
+require_relative './lib/custom_commands_generator'
+require_relative './lib/custom_commands'
 Dotenv.load
 
 bot = Discordrb::Commands::CommandBot.new token: ENV['TOKEN'], application_id: ENV['APPID'], prefix: '!'
-bot.include! PictureCommands
 bot.include! RaidCommands
 bot.include! Messages
 
-bot.command(:idolsheet, chain_usable: false) do |event|
-  event.respond "https://docs.google.com/spreadsheets/d/13xphGFupxzXDW0Y4L0-4Da60buzetWfW_v5lonwZWG0/edit#gid=0"
-end
+custom_commands = CustomCommandGenerator.load_commands(CustomCommands)
+bot.include! custom_commands
 
-bot.command(:superspecial, chain_usable: false) do |event|
-  event.respond "https://www.youtube.com/watch?v=TUmm1QjLC5U"
+bot.command(:add_command,
+            chain_usable: false,
+            description: "Adds or updates a custom command. Usage: !add_command trigger type url. Example: !add_command stick img url_to_picture. Please keep it clean and SFW."
+            ) do |event, *args|
+
+  trigger = args[0]
+  type = args[1]
+  url = args[2]
+
+  unless args.size != 3
+    command = CustomCommandGenerator.new(trigger, type, url, event.user.name)
+    message = command.add
+    new_commands = CustomCommandGenerator.load_commands(CustomCommands)
+    bot.include! new_commands
+    event.respond "#{message}"
+  else
+    event.respond "Missing arguments. Check !help add_command for more info."
+  end
 end
 
 bot.run
