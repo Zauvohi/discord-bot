641055a6264f3cbb1bd2e001aef408cf569061ef
Zian <zauvohi@gmail>
2017-05-06 18:22:24 -0500

    Added base commands for AnimeScheduler

--- lib/anime_commands.rb
+++ lib/anime_commands.rb
@@ -2,4 +2,39 @@ module AnimeCommands
   extend Discordrb::Commands::CommandContainer
   require_relative 'anime_scheduler'
   @anime_scheduler = AnimeScheduler.new
+
+  command(
+  :anime_today,
+  chain_usable: false,
+  description: "Returns a list of the stations anime airing today"
+  ) do |event|
+  end
+
+  command(
+  :anime_next,
+  chain_usable: false,
+  description: "Returns the next airing day"
+  ) do |event|
+  end
+
+  command(
+  :anime_sub,
+  chain_usable: false,
+  description: "Subscribe the current channel to the anime scheduler."
+  ) do |event|
+  end
+
+  command(
+  :anime_unsub,
+  chain_usable: false,
+  description: "Unsubscribe the current channelfrom the anime scheduler."
+  ) do |event|
+  end
+
+  command(
+  :anime_timetable,
+  chain_usable: false,
+  description: "Returns a list of the timetable"
+  ) do |event|
+  end
 end
